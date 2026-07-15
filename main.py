99161058f6d3aceb5fd06db1cfe7e77abd946ec5
elemermelada <iiasanmi9@gmail.com>
2026-07-16 01:55:11 +0200

    feat(update): run update script also on main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 from datetime import datetime
 from logger import setup_logging
+from dotenv import load_dotenv
+from time import sleep
+
+load_dotenv()
 
 os.makedirs("logs", exist_ok=True)
 log_file = f"logs/run_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
@@ -9,7 +13,10 @@ setup_logging(log_file)
 from request import request
 from retrieve import retrieve
 from process import process
+from update import update
 
 request()
+sleep(5)
 retrieve()
 process()
+update()
